a1b12c14e7f1c65833b6a5f87f38170fdb185367
avinashvalechha-creator <avinashvalechha@gmail.com>
2026-05-13 11:31:46 +0530

    Add CalcHelper class with greeting and add methods

---
+++ app/src/main/java/com/example/calc/CalcHelper.java
@@ -0,0 +1,11 @@
+package com.example.calc;
+
+public class CalcHelper {
+    public String getGreeting() {
+        return "Kotlin + Java Calc";
+    }
+
+    public double add(double a, double b) {
+        return a + b;
+    }
+}
